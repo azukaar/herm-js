6d2fb8bb47123bedc021d4cf43b7988957120c06
Yann Stepienik <azukaar@gmail.com>
2018-04-04 17:38:40 +0100

    chore: jest  lint

--- packages/herm-js-document/test/json.spec.js
+++ packages/herm-js-document/test/json.spec.js
@@ -1,29 +1,29 @@
 import HermJsonDoc from '../modules/json';
 
 describe('JSON', () => {
-    test('Is able push JSON', () => {
-        let docJson = new HermJsonDoc();
-        docJson.key = "123";
-        expect(docJson.key).toBe("123");
-    });
+  test('Is able push JSON', () => {
+    const docJson = new HermJsonDoc();
+    docJson.key = '123';
+    expect(docJson.key).toBe('123');
+  });
 
-    test('Is able parse to JSON', () => {
-        let docJson = new HermJsonDoc();
-        docJson.key = "123";
-        expect(docJson.display()).toEqual({key: "123"});
-    });
-    
-    test('Is able merge JSON', () => {
-        let docJson = new HermJsonDoc();
-        docJson.key = "123";
-        expect(docJson.display()).toEqual({key: "123"});
+  test('Is able parse to JSON', () => {
+    const docJson = new HermJsonDoc();
+    docJson.key = '123';
+    expect(docJson.display()).toEqual({ key: '123' });
+  });
 
-        let docJson2 = docJson.duplicate();
-        docJson2.key = "5";
-        docJson2.key2 = "7";
+  test('Is able merge JSON', () => {
+    let docJson = new HermJsonDoc();
+    docJson.key = '123';
+    expect(docJson.display()).toEqual({ key: '123' });
 
-        docJson = docJson.merge(docJson2);
+    const docJson2 = docJson.duplicate();
+    docJson2.key = '5';
+    docJson2.key2 = '7';
 
-        expect(docJson.display()).toEqual({key: "5", key2: "7"});
-    });
+    docJson = docJson.merge(docJson2);
+
+    expect(docJson.display()).toEqual({ key: '5', key2: '7' });
+  });
 });
--- packages/herm-js-document/test/text.spec.js
+++ packages/herm-js-document/test/text.spec.js
@@ -1,316 +1,316 @@
 import HermDoc from '../modules/index';
 
 describe('Text', () => {
-    test('Is able push text', () => {
-        let doc = new HermDoc();
-        doc.push(0, 'Hello');
-        expect(doc.display()).toBe('Hello');
+  test('Is able push text', () => {
+    const doc = new HermDoc();
+    doc.push(0, 'Hello');
+    expect(doc.display()).toBe('Hello');
+  });
+
+  test('Is able push text at location', () => {
+    const doc = new HermDoc();
+    doc.push(0, 'Hello');
+    doc.push(3, 'you');
+    expect(doc.display()).toBe('Helyoulo');
+  });
+
+  test('Is able to duplicate', () => {
+    const doc = new HermDoc();
+    doc.push(0, 'Hello');
+    const doc2 = doc.duplicate();
+    expect(doc2.keys).toEqual(doc.keys);
+  });
+
+  test('Is able to merge', () => {
+    let doc = new HermDoc();
+    doc.push(0, 'Hello');
+
+    const doc2 = doc.duplicate();
+    doc.push(6, ' world');
+    doc2.push(6, ' !');
+
+    expect(doc.display()).toBe('Hello world');
+    expect(doc2.display()).toBe('Hello !');
+
+    doc = doc.merge(doc2); // first merge
+    expect(doc.display()).toBe('Hello world !');
+
+    doc2.push(7, '?');
+    expect(doc2.display()).toBe('Hello ?!');
+
+    doc = doc.merge(doc2); // no duplicates, even with outdated documents
+    expect(doc.display()).toBe('Hello world ?!');
+  });
+
+  test('Is able to display range', () => {
+    const doc = new HermDoc('123456');
+    expect(doc.displayRange(0, 3)).toBe('123');
+  });
+
+  describe('at document start', () => {
+    it('merge into itself, empty', () => {
+      const doc = new HermDoc();
+
+      doc.merge(doc);
+      doc.merge(doc);
+      doc.merge(doc);
+
+      expect(doc.display()).toBe('');
     });
 
-    test('Is able push text at location', () => {
-        let doc = new HermDoc();
-        doc.push(0, 'Hello');
-        doc.push(3, 'you');
-        expect(doc.display()).toBe('Helyoulo');
+    it('merge into itself, initial text', () => {
+      const doc = new HermDoc('foobar');
+
+      doc.merge(doc);
+      doc.merge(doc);
+      doc.merge(doc);
+
+      expect(doc.display()).toBe('foobar');
     });
 
-    test('Is able to duplicate', () => {
-        let doc = new HermDoc();
-        doc.push(0, 'Hello');
-        let doc2 = doc.duplicate();
-        expect(doc2.keys).toEqual(doc.keys);
+    it('three empty doc merge', () => {
+      let doc1 = new HermDoc();
+      let doc2 = new HermDoc();
+      let doc3 = new HermDoc();
+
+      doc1 = doc1.merge(doc2);
+      doc1 = doc1.merge(doc3);
+
+      doc2 = doc2.merge(doc1);
+      doc2 = doc2.merge(doc3);
+
+      doc3 = doc3.merge(doc1);
+      doc3 = doc3.merge(doc1);
+
+      expect(doc1.display()).toBe('');
+      expect(doc2.display()).toBe('');
+      expect(doc3.display()).toBe('');
+
+      doc1 = doc1.merge(doc2);
+      doc1 = doc1.merge(doc3);
+
+      doc2 = doc2.merge(doc1);
+      doc2 = doc2.merge(doc3);
+
+      doc3 = doc3.merge(doc1);
+      doc3 = doc3.merge(doc1);
+
+      expect(doc1.display()).toBe('');
+      expect(doc2.display()).toBe('');
+      expect(doc3.display()).toBe('');
     });
 
-    test('Is able to merge', () => {
-        let doc = new HermDoc();
-        doc.push(0, 'Hello');
+    it('three non-empty doc merge', () => {
+      let doc1 = new HermDoc('a');
+      let doc2 = new HermDoc('b');
+      let doc3 = new HermDoc('c');
 
-        let doc2 = doc.duplicate();
-        doc.push(6, ' world');
-        doc2.push(6, ' !');
+      doc1 = doc1.merge(doc2);
+      doc1 = doc1.merge(doc3);
 
-        expect(doc.display()).toBe('Hello world');
-        expect(doc2.display()).toBe('Hello !');
+      doc2 = doc2.merge(doc1);
+      doc2 = doc2.merge(doc3);
 
-        doc = doc.merge(doc2); // first merge
-        expect(doc.display()).toBe('Hello world !');
+      doc3 = doc3.merge(doc1);
+      doc3 = doc3.merge(doc2);
 
-        doc2.push(7, '?');
-        expect(doc2.display()).toBe('Hello ?!');
+      expect(doc1.display()).toBe('abc');
+      expect(doc2.display()).toBe('abc');
+      expect(doc3.display()).toBe('abc');
+    });
+  });
 
-        doc = doc.merge(doc2); // no duplicates, even with outdated documents
-        expect(doc.display()).toBe('Hello world ?!');
+  describe('is able to delete', () => {
+    describe('at start', () => {
+      test('empty string - 1', () => {
+        const doc = new HermDoc('123456');
+        doc.deleteRange(0, 0);
+        expect(doc.display()).toBe('123456');
+      });
+
+      test('empty string - 2', () => {
+        const doc = new HermDoc('');
+        doc.deleteRange(0, 0);
+        expect(doc.display()).toBe('');
+      });
+
+      test('empty string - 3', () => {
+        const doc = new HermDoc();
+        doc.deleteRange(0, 0);
+        expect(doc.display()).toBe('');
+      });
+
+      test('empty string - 4', () => {
+        const doc = new HermDoc('foo');
+        doc.deleteRange(0, 0);
+        expect(doc.display()).toBe('foo');
+      });
+
+      test('empty string - 5', () => {
+        const doc = new HermDoc('');
+        doc.deleteRange(0, 9999);
+        expect(doc.display()).toBe('');
+      });
+
+      test('empty string - 6', () => {
+        const doc = new HermDoc();
+        doc.deleteRange(0, 9999);
+        doc.deleteRange(0, 9999);
+        expect(doc.display()).toBe('');
+      });
+
+      test('empty string - 7', () => {
+        const doc = new HermDoc('');
+        doc.deleteRange(9999, 9999);
+        doc.deleteRange(9999, 9999);
+        expect(doc.display()).toBe('');
+      });
+
+      test('empty string - 8', () => {
+        const doc = new HermDoc();
+        doc.deleteRange(9999, 9999);
+        doc.deleteRange(9999, 9999);
+        expect(doc.display()).toBe('');
+      });
+
+      test('empty string - 9', () => {
+        const doc = new HermDoc('foobar');
+        doc.deleteRange(9999, 0);
+        expect(doc.display()).toBe('foobar');
+      });
+
+      test('empty string - 10', () => {
+        const doc = new HermDoc();
+        doc.deleteRange(9999, 0);
+        doc.deleteRange(9999, 0);
+        expect(doc.display()).toBe('');
+      });
+
+      test('empty string - 11', () => {
+        const doc = new HermDoc('');
+        doc.deleteRange(9999, 0);
+        doc.deleteRange(9999, 0);
+        expect(doc.display()).toBe('');
+      });
+
+      test('non-empty string - 1', () => {
+        const doc = new HermDoc('123456');
+        doc.deleteRange(0, 3);
+        expect(doc.display()).toBe('456');
+
+        doc.deleteRange(0, 3);
+        expect(doc.display()).toBe('');
+      });
+
+      test('non-empty string - 2', () => {
+        const doc = new HermDoc('123456');
+        doc.deleteRange(0, 3);
+        expect(doc.display()).toBe('456');
+
+        doc.deleteRange(0, 1);
+        expect(doc.display()).toBe('56');
+
+        doc.deleteRange(0, 1);
+        expect(doc.display()).toBe('6');
+
+        doc.deleteRange(0, 1);
+        expect(doc.display()).toBe('');
+
+        doc.deleteRange(0, 1);
+        expect(doc.display()).toBe('');
+
+        doc.deleteRange(0, 1);
+        expect(doc.display()).toBe('');
+      });
+
+      test('non-empty string - 3', () => {
+        const doc = new HermDoc('123456');
+        doc.deleteRange(0, 3);
+        expect(doc.display()).toBe('456');
+
+        doc.deleteRange(0, 9999);
+        expect(doc.display()).toBe('');
+
+        doc.deleteRange(0, 9999);
+        expect(doc.display()).toBe('');
+      });
     });
 
-    test('Is able to display range', () => {
-        let doc = new HermDoc('123456');
-        expect(doc.displayRange(0, 3)).toBe('123');
+    describe('in the middle', () => {
+      test('empty string', () => {
+        const doc = new HermDoc('123456');
+        doc.deleteRange(3, 0);
+        expect(doc.display()).toBe('123456');
+        doc.deleteRange(3, 0);
+        doc.deleteRange(3, 0);
+        expect(doc.display()).toBe('123456');
+      });
+
+      test('non-empty string - 1', () => {
+        const doc = new HermDoc('123456');
+        doc.deleteRange(3, 0);
+        expect(doc.display()).toBe('123456');
+        doc.deleteRange(3, 1);
+        expect(doc.display()).toBe('12356');
+        doc.deleteRange(3, 1);
+        expect(doc.display()).toBe('1236');
+      });
+
+      test('non-empty string - 2', () => {
+        const doc = new HermDoc('123456');
+        doc.deleteRange(1, 4);
+        expect(doc.display()).toBe('16');
+      });
     });
 
-    describe('at document start', () => {
-        it('merge into itself, empty', () => {
-            const doc = new HermDoc();
+    describe('in the end', () => {
+      test('behind range', () => {
+        const doc = new HermDoc('123456');
+        doc.deleteRange(6, 1);
+        expect(doc.display()).toBe('123456');
+        doc.deleteRange(9999, 9999);
+        expect(doc.display()).toBe('123456');
+      });
 
-            doc.merge(doc);
-            doc.merge(doc);
-            doc.merge(doc);
+      test('empty string', () => {
+        const doc = new HermDoc('123456');
+        doc.deleteRange(5, 0);
+        expect(doc.display()).toBe('123456');
+        doc.deleteRange(5, 0);
+        expect(doc.display()).toBe('123456');
+        doc.deleteRange(5, -1);
+        expect(doc.display()).toBe('123456');
+      });
 
-            expect(doc.display()).toBe('');
-        });
+      test('last char', () => {
+        const doc = new HermDoc('123456');
+        doc.deleteRange(5, 1);
+        expect(doc.display()).toBe('12345');
+        doc.deleteRange(5, 1);
+        expect(doc.display()).toBe('12345');
+      });
 
-        it('merge into itself, initial text', () => {
-            const doc = new HermDoc('foobar');
+      test('last char twice', () => {
+        const doc = new HermDoc('123456');
+        doc.deleteRange(5, 1);
+        expect(doc.display()).toBe('12345');
+        doc.deleteRange(4, 1);
+        expect(doc.display()).toBe('1234');
+      });
 
-            doc.merge(doc);
-            doc.merge(doc);
-            doc.merge(doc);
-
-            expect(doc.display()).toBe('foobar');
-        });
-
-        it('three empty doc merge', () => {
-            let doc1 = new HermDoc();
-            let doc2 = new HermDoc();
-            let doc3 = new HermDoc();
-
-            doc1 = doc1.merge(doc2);
-            doc1 = doc1.merge(doc3);
-
-            doc2 = doc2.merge(doc1);
-            doc2 = doc2.merge(doc3);
-
-            doc3 = doc3.merge(doc1);
-            doc3 = doc3.merge(doc1);
-
-            expect(doc1.display()).toBe('');
-            expect(doc2.display()).toBe('');
-            expect(doc3.display()).toBe('');
-
-            doc1 = doc1.merge(doc2);
-            doc1 = doc1.merge(doc3);
-
-            doc2 = doc2.merge(doc1);
-            doc2 = doc2.merge(doc3);
-
-            doc3 = doc3.merge(doc1);
-            doc3 = doc3.merge(doc1);
-
-            expect(doc1.display()).toBe('');
-            expect(doc2.display()).toBe('');
-            expect(doc3.display()).toBe('');
-        });
-
-        it('three non-empty doc merge', () => {
-            let doc1 = new HermDoc('a');
-            let doc2 = new HermDoc('b');
-            let doc3 = new HermDoc('c');
-
-            doc1 = doc1.merge(doc2);
-            doc1 = doc1.merge(doc3);
-
-            doc2 = doc2.merge(doc1);
-            doc2 = doc2.merge(doc3);
-
-            doc3 = doc3.merge(doc1);
-            doc3 = doc3.merge(doc2);
-
-            expect(doc1.display()).toBe('abc');
-            expect(doc2.display()).toBe('abc');
-            expect(doc3.display()).toBe('abc');
-        });
+      test('last two chars', () => {
+        const doc = new HermDoc('123456');
+        doc.deleteRange(4, 2);
+        expect(doc.display()).toBe('1234');
+        doc.deleteRange(2, 2);
+        expect(doc.display()).toBe('12');
+        doc.deleteRange(0, 2);
+        expect(doc.display()).toBe('');
+        doc.deleteRange(0, 2);
+        expect(doc.display()).toBe('');
+        doc.deleteRange(0, 9999);
+        expect(doc.display()).toBe('');
+      });
     });
-
-    describe('is able to delete', () => {
-        describe('at start', () => {
-            test('empty string - 1', () => {
-                const doc = new HermDoc('123456');
-                doc.deleteRange(0, 0);
-                expect(doc.display()).toBe('123456');
-            });
-
-            test('empty string - 2', () => {
-                const doc = new HermDoc('');
-                doc.deleteRange(0, 0);
-                expect(doc.display()).toBe('');
-            });
-
-            test('empty string - 3', () => {
-                const doc = new HermDoc();
-                doc.deleteRange(0, 0);
-                expect(doc.display()).toBe('');
-            });
-
-            test('empty string - 4', () => {
-                const doc = new HermDoc('foo');
-                doc.deleteRange(0, 0);
-                expect(doc.display()).toBe('foo');
-            });
-
-            test('empty string - 5', () => {
-                const doc = new HermDoc('');
-                doc.deleteRange(0, 9999);
-                expect(doc.display()).toBe('');
-            });
-
-            test('empty string - 6', () => {
-                const doc = new HermDoc();
-                doc.deleteRange(0, 9999);
-                doc.deleteRange(0, 9999);
-                expect(doc.display()).toBe('');
-            });
-
-            test('empty string - 7', () => {
-                const doc = new HermDoc('');
-                doc.deleteRange(9999, 9999);
-                doc.deleteRange(9999, 9999);
-                expect(doc.display()).toBe('');
-            });
-
-            test('empty string - 8', () => {
-                const doc = new HermDoc();
-                doc.deleteRange(9999, 9999);
-                doc.deleteRange(9999, 9999);
-                expect(doc.display()).toBe('');
-            });
-
-            test('empty string - 9', () => {
-                const doc = new HermDoc('foobar');
-                doc.deleteRange(9999, 0);
-                expect(doc.display()).toBe('foobar');
-            });
-
-            test('empty string - 10', () => {
-                const doc = new HermDoc();
-                doc.deleteRange(9999, 0);
-                doc.deleteRange(9999, 0);
-                expect(doc.display()).toBe('');
-            });
-
-            test('empty string - 11', () => {
-                const doc = new HermDoc('');
-                doc.deleteRange(9999, 0);
-                doc.deleteRange(9999, 0);
-                expect(doc.display()).toBe('');
-            });
-
-            test('non-empty string - 1', () => {
-                let doc = new HermDoc('123456');
-                doc.deleteRange(0, 3);
-                expect(doc.display()).toBe('456');
-
-                doc.deleteRange(0, 3);
-                expect(doc.display()).toBe('');
-            });
-
-            test('non-empty string - 2', () => {
-                let doc = new HermDoc('123456');
-                doc.deleteRange(0, 3);
-                expect(doc.display()).toBe('456');
-
-                doc.deleteRange(0, 1);
-                expect(doc.display()).toBe('56');
-
-                doc.deleteRange(0, 1);
-                expect(doc.display()).toBe('6');
-
-                doc.deleteRange(0, 1);
-                expect(doc.display()).toBe('');
-
-                doc.deleteRange(0, 1);
-                expect(doc.display()).toBe('');
-
-                doc.deleteRange(0, 1);
-                expect(doc.display()).toBe('');
-            });
-
-            test('non-empty string - 3', () => {
-                let doc = new HermDoc('123456');
-                doc.deleteRange(0, 3);
-                expect(doc.display()).toBe('456');
-
-                doc.deleteRange(0, 9999);
-                expect(doc.display()).toBe('');
-
-                doc.deleteRange(0, 9999);
-                expect(doc.display()).toBe('');
-            });
-        });
-
-        describe('in the middle', () => {
-            test('empty string', () => {
-                const doc = new HermDoc('123456');
-                doc.deleteRange(3, 0);
-                expect(doc.display()).toBe('123456');
-                doc.deleteRange(3, 0);
-                doc.deleteRange(3, 0);
-                expect(doc.display()).toBe('123456');
-            });
-
-            test('non-empty string - 1', () => {
-                const doc = new HermDoc('123456');
-                doc.deleteRange(3, 0);
-                expect(doc.display()).toBe('123456');
-                doc.deleteRange(3, 1);
-                expect(doc.display()).toBe('12356');
-                doc.deleteRange(3, 1);
-                expect(doc.display()).toBe('1236');
-            });
-
-            test('non-empty string - 2', () => {
-                const doc = new HermDoc('123456');
-                doc.deleteRange(1, 4);
-                expect(doc.display()).toBe('16');
-            });
-        });
-
-        describe('in the end', () => {
-            test('behind range', () => {
-                const doc = new HermDoc('123456');
-                doc.deleteRange(6, 1);
-                expect(doc.display()).toBe('123456');
-                doc.deleteRange(9999, 9999);
-                expect(doc.display()).toBe('123456');
-            });
-
-            test('empty string', () => {
-                const doc = new HermDoc('123456');
-                doc.deleteRange(5, 0);
-                expect(doc.display()).toBe('123456');
-                doc.deleteRange(5, 0);
-                expect(doc.display()).toBe('123456');
-                doc.deleteRange(5, -1);
-                expect(doc.display()).toBe('123456');
-            });
-
-            test('last char', () => {
-                const doc = new HermDoc('123456');
-                doc.deleteRange(5, 1);
-                expect(doc.display()).toBe('12345');
-                doc.deleteRange(5, 1);
-                expect(doc.display()).toBe('12345');
-            });
-
-            test('last char twice', () => {
-                const doc = new HermDoc('123456');
-                doc.deleteRange(5, 1);
-                expect(doc.display()).toBe('12345');
-                doc.deleteRange(4, 1);
-                expect(doc.display()).toBe('1234');
-            });
-
-            test('last two chars', () => {
-                const doc = new HermDoc('123456');
-                doc.deleteRange(4, 2);
-                expect(doc.display()).toBe('1234');
-                doc.deleteRange(2, 2);
-                expect(doc.display()).toBe('12');
-                doc.deleteRange(0, 2);
-                expect(doc.display()).toBe('');
-                doc.deleteRange(0, 2);
-                expect(doc.display()).toBe('');
-                doc.deleteRange(0, 9999);
-                expect(doc.display()).toBe('');
-            });
-        });
-    });
+  });
 });
